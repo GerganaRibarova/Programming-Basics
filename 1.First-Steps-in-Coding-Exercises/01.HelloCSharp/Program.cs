2ea1d6ec504d4e8626010e98dad60890cbbebc29
GerganaRibarova <geri.ribarova@gmail.com>
2017-01-21 23:29:10 +0200

    Add a folder

---
+++ 1.First-Steps-in-Coding-Exercises/01.HelloCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.HelloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello C#");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1.First-Steps-in-Coding-Exercises/07.Sumator/07.Sumator/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07.Sumator
+{
+    public partial class FormCalculate : Form
+    {
+        public FormCalculate()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num1 = int.Parse(this.textBox1.Text);
+                int num2 = int.Parse(this.textBox2.Text);
+                int sum = num1 + num2;
+                textBoxSum.Text = sum.ToString();
+            }
+            catch(Exception)
+            {
+                textBoxSum.Text = "error";
+            }
+            
+
+        }
+    }
+}
